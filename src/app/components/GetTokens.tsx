693df04962d52991e2cadc5e9272e77742b49e30
NeelContractor <neelcontractor7@gmail.com>
2024-09-27 21:12:12 +0530

    fix lint errors

--- src/app/components/GetTokens.tsx
+++ src/app/components/GetTokens.tsx
@@ -40,7 +40,7 @@ const GetTokens: React.FC = () => {
                     const tokenSymbol = metadata?.symbol || "N/A";
                     const tokenUri = metadata?.uri || "";
 
-                    console.log("uri: ", tokenUri);
+                    // console.log("uri: ", tokenUri);
 
                     return {
                         mint: mintAddress,
