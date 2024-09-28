9ebfd4b78df030e8654491a962c43c91db3af0e6
NeelContractor <neelcontractor7@gmail.com>
2024-09-28 10:20:30 +0530

    removed few unwanted pages

--- src/app/components/GetTokens.tsx
+++ src/app/components/GetTokens.tsx
@@ -40,7 +40,7 @@ const GetTokens: React.FC = () => {
                     const tokenSymbol = metadata?.symbol || "N/A";
                     const tokenUri = metadata?.uri || "";
 
-                    // console.log("uri: ", tokenUri);
+                    console.log("uri: ", tokenUri); // outputs: https://cdn.100xdevs.com/metadata.json
 
                     return {
                         mint: mintAddress,
@@ -58,42 +58,44 @@ const GetTokens: React.FC = () => {
     }
 
     return (
-        <div className="grid border rounded-lg drop-shadow-md p-10">
-            <div className="flex justify-center">
-                <input 
-                    type="text" 
-                    className="border rounded-lg text-base font-semibold p-2 mb-3 outline-none w-8/12" 
-                    placeholder="Enter your PublicKey here."
-                    onChange={(e) => setPublicKey(e.target.value)}
-                />
-            </div>
-            <div className="flex justify-center">
-                <button 
-                    className="border bg-blue-600 text-base font-semibold rounded-lg p-3"
-                    onClick={fetchTokensByOwner}
-                >Get Tokens</button>
-            </div>
-            <div className="grid justify-center ">
-                {tokenData?.length ? (
-                    tokenData.map((value, index) => (
-                        <React.Fragment key={index}>
-                            <div className="flex border rounded-lg border-gray-400 p-3 m-2">
-                                <div className="w-40 truncate">
-                                    <a href={value.uri} target="_blank" rel="noopener noreferrer" className="flex items-center">{value.uri}</a>
+        <>
+            <div className="grid border rounded-lg drop-shadow-md p-10">
+                <div className="flex justify-center">
+                    <input 
+                        type="text" 
+                        className="border rounded-lg text-base font-semibold p-2 mb-3 outline-none w-8/12" 
+                        placeholder="Enter your PublicKey here."
+                        onChange={(e) => setPublicKey(e.target.value)}
+                    />
+                </div>
+                <div className="flex justify-center">
+                    <button 
+                        className="border bg-blue-600 text-base font-semibold rounded-lg p-3"
+                        onClick={fetchTokensByOwner}
+                    >Get Tokens</button>
+                </div>
+                <div className="grid justify-center ">
+                    {tokenData?.length ? (
+                        tokenData.map((value, index) => (
+                            <React.Fragment key={index}>
+                                <div className="flex border rounded-lg border-gray-400 p-3 m-2">
+                                    <div className="w-40 truncate">
+                                        <a href={value.uri} target="_blank" rel="noopener noreferrer" className="flex items-center">{value.uri}</a>
+                                    </div>
+                                    <div className="ml-4">
+                                        <a href={value.mint} className="text-base font-bold">Token Name: {value.name}</a>
+                                        <p className="text-base font-semibold">Token Amount: {value.amount} {value.symbol}</p>
+                                    </div>
                                 </div>
-                                <div className="ml-4">
-                                    <a href={value.mint} className="text-base font-bold">Token Name: {value.name}</a>
-                                    <p className="text-base font-semibold">Token Amount: {value.amount} {value.symbol}</p>
-                                </div>
-                            </div>
-                        <br />
-                        </React.Fragment>
-                    ))
-                ) : (
-                    <p className="font-bold text-base text-slate-400 m-2">No tokens found for this wallet.</p>
-                )}
+                            <br />
+                            </React.Fragment>
+                        ))
+                    ) : (
+                        <p className="font-bold text-base text-slate-400 m-2">No tokens found for this wallet.</p>
+                    )}
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
